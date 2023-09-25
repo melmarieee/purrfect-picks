5ffdda3a6021c78cbc41587c9d4e2243a97071cb
Mel Marie Resurreccion <melmarieresurreccion@Mels-MacBook-Air.local>
2023-09-25 22:32:55 +0800

    Apply Route

--- purrfect-picks/src/App.jsx
+++ purrfect-picks/src/App.jsx
@@ -1,14 +1,22 @@
+import ReactDom from 'react-dom/client'
+import {BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 import Login from './components/login'
+import Signup from './components/signup'
+import Account from './components/account'
 
 function App() {
 
   return (
     <>
       <div>
-        {/* <Signup/> */}
-        {/* <Login/> */}
-        {/* { <index/>} */}
+        <BrowserRouter>
+          <Routes>
+              <Route path="/login" element={<Login/>} />
+              <Route path="/signup" element={<Signup/>} />
+              <Route path="/account" element={<Account/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
